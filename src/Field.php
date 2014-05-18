b05a8cacc892707d6a55813dadc973188d55c6e9
Daniel G Sims <danielsims@quickenloans.com>
2014-05-18 10:51:15 -0400

    HTML input types are not enforced

--- src/Field.php
+++ src/Field.php
@@ -7,24 +7,12 @@ class Field
     private $value;
     private $title;
 
-    //should allowed types be enforced. check the spec for MUST or SHOULD
-    private $allowedTypes = array(
-        "hidden", "text", "search", "tel", "url", "email",
-        "password", "datetime", "date", "month", "week",
-        "time", "datetime-local", "number", "range", "color",
-        "checkbox", "radio", "file", "image", "reset", "button"
-    );
-
     public function __construct($name, $type = "text", $title = null, $value = null)
     {
-        if (!in_array($type,$this->allowedTypes)) {
-            throw new \InvalidArgumentException("Invalid field type");
-        }
-
-        $this->name = $name;
-        $this->type = $type;
-        $this->value = $value;
-        $this->title = $title;
+        $this->name = (string) $name;
+        $this->type = (string) $type;
+        $this->value = (string) $value;
+        $this->title = (string) $title;
 
     }
 
