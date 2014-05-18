d6f4d21cc09ac6fba333edbd13116bc17d7a2d62
Daniel G Sims <danielsims@quickenloans.com>
2014-05-18 17:58:27 -0400

    Example to mach Siren spec example

--- index.php
+++ index.php
@@ -1,33 +1,83 @@
 <?php
 
     require_once "vendor/autoload.php";
+    use Siren\Components\Entity;
+    use Siren\Components\Link;
+    use Siren\Components\Action;
+    use Siren\Components\Field;
+    use Siren\Encoder;
 
-    $l = new Siren\Components\Link(
+    $order = new Entity();
+    $order->setClass(array("order"));
+    $order->setProperties(array(
+        "orderNumber" => 42,
+        "itemCount" => 3,
+        "status" => "pending"
+    ));
+
+    $items = new Entity;
+    $items->setClass(array("items","collection"));
+    $items->setRel(array("http://x.io/rels/order-items"));
+    $items->setHref(array("http://api.x.io/orders/42/items"));
+    $order->addEntity($items);
+
+    $customerInfo = new Entity;
+    $customerInfo->setClass(array("info","customer"));
+    $customerInfo->setRel(array("http://x.io/rels/customer"));
+    $customerInfo->setProperties(array(
+        "customerId" => "pj123",
+        "name" => "Peter Joseph"
+    ));
+
+    $customerInfo->addLink(new Link(
         array("self"),
-        "http://test.com/api",
-        "Index"
+        "http://api.x.io/customers/pj123" 
+    ));
+
+    $order->addEntity($customerInfo);
+
+    $fields = array();
+    $fields[] = new Field(
+        "orderNumber",
+        "hidden",
+        null,
+        42
     );
 
-    $en = new Siren\Components\Entity();
-    $en->setClass(array("order"));
-    $en->setProperties(array(
-        'orderNumber'=> 42,
-        'itemCount'=> 3,
-        'status'=> "pending"
-    )); 
+    $fields[] = new Field("productCode");
+    $fields[] = new Field("quantity", "number");
 
-    $f = new Siren\Components\Field("product");
-    $a = new Siren\Components\Action(
+    $action = new Action(
         "add-item",
-        "http://mysite.com/items",
+        "http://api.x.io/orders/42/items",
         "POST",
         "Add Item",
-        array(),
         null,
-        array($f)
+        null,
+        $fields
     );
 
-    $en->addAction($a);
+    $order->addAction($action);
 
-    $e = new Siren\Encoder;
-    print_r($e->encode($en));
+    $links = array();
+    $links[] = new Link(
+        array("self"),
+        "http://api.x.io/orders/42"
+    );
+
+    $links[] = new Link(
+        array("previous"),
+        "http://api.x.io/orders/41"
+    );
+
+    $links[] = new Link(
+        array("next"),
+        "http://api.x.io/orders/43"
+    );
+
+    $order->setLinks($links);
+
+    $encoder = new Encoder;
+    $response = $encoder->encode($order);
+
+    print_r($response);
